6d4eae509d1e6adb29a2c07ec93c4071c23a73cb
Alexander Guschin <igushin1999@gmail.com>
2019-10-16 16:01:35 +0300

    fix1

--- modules/task_1/guschin_a_word_count/word_count.h
+++ modules/task_1/guschin_a_word_count/word_count.h
@@ -4,7 +4,7 @@
 #include <string>
 
 bool isLetter(char sym);
-int getCount(std::string st);
-int getLinearCount(std::string st, int start, int end);
+int getCount(std::string st, int st_size);
+int getLinearCount(std::string st);
 
 #endif  // MODULES_TASK_1_GUSCHIN_A_WORD_COUNT_WORD_COUNT_H_
