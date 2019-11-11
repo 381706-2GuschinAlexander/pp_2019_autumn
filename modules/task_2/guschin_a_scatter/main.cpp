0851a28819c1abfa66618385c94b1ffa16e626e9
Alexander Guschin <igushin1999@gmail.com>
2019-11-11 22:37:37 +0300

    scatter + 1 test

--- modules/task_2/guschin_a_scatter/main.cpp
+++ modules/task_2/guschin_a_scatter/main.cpp
@@ -6,10 +6,28 @@
 
 
 
-TEST(scatter, placeholder) {
-    ASSERT_EQ(true, false);
-}
+TEST(scatter, can_scatter_and_gather) {
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  int *p, *d;
+  int dest[3];
+  if (rank == 0) {
+    p = new int[3 * size];
+    d = new int[3 * size];
+    for (int i = 0; i < 3 * size; ++i) p[i] = i;
+  }
+  MPI_Scatter_custom(p, 3, MPI_INT, &dest[0], 3, MPI_INT, 0, MPI_COMM_WORLD);
+  EXPECT_EQ(1, 1);
+  MPI_Gather(&dest[0], 3, MPI_INT, d, 3, MPI_INT, 0, MPI_COMM_WORLD);
 
+  if (rank == 0) {
+    int isEq = 1;
+    for (int i = 0; i < 3 * size; ++i)
+      if (d[i] != i) isEq = 0;
+    EXPECT_EQ(isEq, 1);
+  }
+}
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   MPI_Init(&argc, &argv);
--- modules/task_2/guschin_a_scatter/scatter.h
+++ modules/task_2/guschin_a_scatter/scatter.h
@@ -1,8 +1,12 @@
 // Copyright 2019 Guschin Alexander
 #ifndef MODULES_TASK_1_GUSCHIN_A_SCATTER_SCATTER_H_
 #define MODULES_TASK_1_GUSCHIN_A_SCATTER_SCATTER_H_
+#include <mpi.h>
 #include <string>
 #include <vector>
 
+int MPI_Scatter_custom(void* sendbuf, int sendcount, MPI_Datatype sendtype,
+                       void* recvbuf, int recvcount, MPI_Datatype recvtype,
+                       int root, MPI_Comm comm);
 
 #endif  // MODULES_TASK_1_GUSCHIN_A_SCATTER_SCATTER_H_
