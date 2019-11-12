2220fb14197b3b39509d95dd90da773727c7864e
Alexander Guschin <igushin1999@gmail.com>
2019-11-12 09:37:53 +0300

    fix for appveyor

--- modules/task_2/guschin_a_scatter/main.cpp
+++ modules/task_2/guschin_a_scatter/main.cpp
@@ -8,13 +8,10 @@ TEST(scatter, throw_when_different_size) {
   int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-  int *p, *d;
+  std::vector<int> p(1);
   int dest[3];
-  if (rank == 0) {
-    p = new int[3 * size];
-    d = new int[3 * size];
-  }
-  EXPECT_EQ(MPI_Scatter_custom(p, 1, MPI_INT, &dest[0], 99, MPI_INT, 0, MPI_COMM_WORLD),
+  
+  EXPECT_EQ(MPI_Scatter_custom(&p[0], 1, MPI_INT, &dest[0], 99, MPI_INT, 0, MPI_COMM_WORLD),
             MPI_ERR_COUNT);
 }
 
@@ -22,20 +19,20 @@ TEST(scatter, can_scatter_and_gather) {
   int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-  int *p, *d;
+  std::vector<int> p(3 * size);
+  std::vector<int> d(3 * size);
   int dest[3];
   if (rank == 0) {
-    p = new int[3 * size];
-    d = new int[3 * size];
     for (int i = 0; i < 3 * size; ++i) p[i] = i;
   }
-  MPI_Scatter_custom(p, 3, MPI_INT, &dest[0], 3, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Gather(&dest[0], 3, MPI_INT, d, 3, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Scatter_custom(&p[0], 3, MPI_INT, &dest[0], 3, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Gather(&dest[0], 3, MPI_INT, &d[0], 3, MPI_INT, 0, MPI_COMM_WORLD);
 
   if (rank == 0) {
     int isEq = 1;
     for (int i = 0; i < 3 * size; ++i)
       if (d[i] != i) isEq = 0;
+
     EXPECT_EQ(isEq, 1);
   }
 }
