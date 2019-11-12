157934a4bab8d200b08130fffd299434dccc3338
Alexander Guschin <igushin1999@gmail.com>
2019-11-12 11:09:48 +0300

    +3 new tests

--- modules/task_2/guschin_a_scatter/main.cpp
+++ modules/task_2/guschin_a_scatter/main.cpp
@@ -15,7 +15,26 @@ TEST(scatter, throw_when_different_size) {
             MPI_ERR_COUNT);
 }
 
-TEST(scatter, can_scatter_and_gather) {
+TEST(scatter, can_scatter_and_gather_double) {
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  std::vector<double> p(3 * size);
+  std::vector<double> d(3 * size);
+  double dest[3];
+  if (rank == 0) {
+    for (int i = 0; i < 3 * size; ++i) p[i] = i + 1.0/i;
+  }
+  MPI_Scatter_custom(&p[0], 3, MPI_DOUBLE, &dest[0], 3, MPI_DOUBLE, 0,
+                     MPI_COMM_WORLD);
+  MPI_Gather(&dest[0], 3, MPI_DOUBLE, &d[0], 3, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+  if (rank == 0) {
+    EXPECT_EQ(p, d);
+  }
+}
+
+TEST(scatter, can_scatter_and_gather_int) {
   int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -25,15 +44,55 @@ TEST(scatter, can_scatter_and_gather) {
   if (rank == 0) {
     for (int i = 0; i < 3 * size; ++i) p[i] = i;
   }
-  MPI_Scatter_custom(&p[0], 3, MPI_INT, &dest[0], 3, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Scatter_custom(&p[0], 3, MPI_INT, &dest[0], 3, MPI_INT, 0,
+                     MPI_COMM_WORLD);
   MPI_Gather(&dest[0], 3, MPI_INT, &d[0], 3, MPI_INT, 0, MPI_COMM_WORLD);
 
   if (rank == 0) {
-    int isEq = 1;
-    for (int i = 0; i < 3 * size; ++i)
-      if (d[i] != i) isEq = 0;
+    EXPECT_EQ(p, d);
+  }
+}
 
-    EXPECT_EQ(isEq, 1);
+TEST(scatter, can_compare_custom_scatter_with_MPI_Scatter) {
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  std::vector<int> p(3 * size);
+  std::vector<int> d1(3 * size);
+  std::vector<int> d2(3 * size);
+  int dest[3];
+  if (rank == 0) {
+    for (int i = 0; i < 3 * size; ++i) p[i] = i;
+  }
+  MPI_Scatter_custom(&p[0], 3, MPI_INT, &dest[0], 3, MPI_INT, 0,
+                     MPI_COMM_WORLD);
+  MPI_Gather(&dest[0], 3, MPI_INT, &d1[0], 3, MPI_INT, 0, MPI_COMM_WORLD);
+
+  MPI_Scatter(&p[0], 3, MPI_INT, &dest[0], 3, MPI_INT, 0,
+                     MPI_COMM_WORLD);
+  MPI_Gather(&dest[0], 3, MPI_INT, &d2[0], 3, MPI_INT, 0, MPI_COMM_WORLD);
+
+  if (rank == 0) {
+    EXPECT_EQ(d1, d2);
+  }
+}
+
+TEST(scatter, can_scatter_and_gather_float) {
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  std::vector<float> p(3 * size);
+  std::vector<float> d(3 * size);
+  float dest[3];
+  if (rank == 0) {
+    for (int i = 0; i < 3 * size; ++i) p[i] = i + 1.0 / i;
+  }
+  MPI_Scatter_custom(&p[0], 3, MPI_FLOAT, &dest[0], 3, MPI_FLOAT, 0,
+                     MPI_COMM_WORLD);
+  MPI_Gather(&dest[0], 3, MPI_FLOAT, &d[0], 3, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+  if (rank == 0) {
+    EXPECT_EQ(p, d);
   }
 }
 
